39b15f86dc4c3f339561cb1e092b73d5fd4d7c79
Mallcock1 <mallcock1@sheffield.ac.uk>
2020-03-26 12:29:40 +0000

    fixed cbar bug

--- vx_contour_interface.py
+++ vx_contour_interface.py
@@ -10,11 +10,14 @@ import scipy as sc
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
+# set global font size
+plt.rcParams.update({'font.size': 12})
+
 # set size of intially perturbed region
 z0 = 1
 
 # set characteristic speeds
-v0 = 1
+v0 = 2
 vp = 1
 vm = 0.5
 vs = sc.sqrt((vm**2 + vp**2)/2)
@@ -52,12 +55,12 @@ xmin = -1
 xmax = -xmin
 zmin = -3*z0
 zmax = -zmin
-Nx = 100
+Nx = 50
 Nz = 100
 
 x = np.linspace(xmin, xmax, Nx)
 z = np.linspace(zmin, zmax, Nz)
-Z, X = np.meshgrid(x, z)
+X, Z = np.meshgrid(x, z)
 
 tstart = 0
 tend = 5
@@ -66,25 +69,27 @@ Nt = 6
 t = np.linspace(tstart, tend, Nt)
 
 # initialise vx array
-vxvals = np.zeros((Nx, Nz, Nt))
+vxvals = np.zeros((Nz, Nx, Nt))
 
 # populate vx array
 for i in range(Nx):
     for j in range(Nz):
         for k in range(Nt):
-            vxvals[i, j, k] = vx(x[i], z[j], t[k])
+            vxvals[j, i, k] = vx(x[i], z[j], t[k])
 
 # Levels for contour plot
-levels = np.linspace(-0.6, 0.6, 101)
+levels = np.linspace(-v0*0.55, v0*0.55, 501)
+
+label_font_size = 20
 
 # plot contours
 fig = plt.figure()
 ax = plt.gca()
 contour = plt.contourf(X, Z, vxvals[:,:,0], levels=levels, cmap='RdBu')
-plt.xlabel(r'$x$')
-plt.ylabel(r'$z$')
+plt.xlabel(r'$x$', fontsize=label_font_size)
+plt.ylabel(r'$z$', fontsize=label_font_size)
 cbar = plt.colorbar()
-cbar.set_label('Velocity', rotation=270)
+cbar.set_label('Velocity', rotation=270, fontsize=label_font_size)
 plt.tight_layout()
 
 ## Fuction to update animation at each time step
@@ -99,14 +104,24 @@ plt.tight_layout()
 #contour_ani.save('contour_animation.mp4', fps=24, extra_args=['-vcodec', 'libx264'])
 
 
-plt.figure
+#plt.figure
 
 
-fig, axs = plt.subplots(3, 2, sharex=True, sharey=True)
+fig, axs = plt.subplots(3, 2, sharex=True, sharey=True, figsize=(10,10))
 contour = axs[0, 0].contourf(X, Z, vxvals[:,:,0], levels=levels, cmap='RdBu')
 axs[0, 1].contourf(X, Z, vxvals[:,:,1], levels=levels, cmap='RdBu')
 axs[1, 0].contourf(X, Z, vxvals[:,:,2], levels=levels, cmap='RdBu')
 axs[1, 1].contourf(X, Z, vxvals[:,:,3], levels=levels, cmap='RdBu')
 axs[2, 0].contourf(X, Z, vxvals[:,:,4], levels=levels, cmap='RdBu')
 axs[2, 1].contourf(X, Z, vxvals[:,:,5], levels=levels, cmap='RdBu')
-fig.colorbar(contour, ax=axs[:, 1])
+
+cbar = fig.colorbar(contour, ax=axs)
+cbar.set_label('Velocity', rotation=270, fontsize=label_font_size, labelpad=10)
+
+axs[2, 0].set_xlabel(r'$x$', fontsize=label_font_size)
+axs[2, 1].set_xlabel(r'$x$', fontsize=label_font_size)
+axs[0, 0].set_ylabel(r'$z$', fontsize=label_font_size)
+axs[1, 0].set_ylabel(r'$z$', fontsize=label_font_size)
+axs[2, 0].set_ylabel(r'$z$', fontsize=label_font_size)
+
+plt.savefig('contour_subplots.png')
